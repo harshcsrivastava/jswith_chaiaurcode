704bac4ea49a3227abb1867682012b6f36c35238
Harsh Chandra Srivastava <139110085+harshcsrivastava@users.noreply.github.com>
2024-02-24 16:44:49 +0000

    TypeConversion

---
+++ 01_basics/03_typeConversion.js
@@ -0,0 +1,36 @@
+let score = 33
+// console.log(typeof score)
+// Typecasting into string
+// Always with first letter capital like Number(){integer}, Boolean(), String()
+
+// let stringScore = String(score)
+// console.log(typeof stringScore)
+
+
+/*
+let Username = "s1e2"
+let toNumber = Number(Username)
+console.log(typeof toNumber)
+console.log(toNumber) 
+*/
+//Converted to Number but value Nan i.e. Not a Number
+// true => 1 and false => 0 and vice versa
+
+// let naam = ""
+// let isBoo = Boolean(naam)
+// console.log(isBoo)
+
+// "" / 0  => False and "AnyValue" / 1 => True
+
+
+// Operations 
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3) //power 2 pow 3
+// console.log(2/2)
+
+console.log(1 + 2 + "2"); //String Last to pahle number add
+console.log("1" + 2 + 2); //String First to string Conversion
+
