d9afb81ab5ae72eabb58ad89d1ac8469902d0dea
Harsh Chandra Srivastava <139110085+harshcsrivastava@users.noreply.github.com>
2024-05-16 14:20:30 +0000

    events and async intro

---
+++ 08_async/async.js
@@ -0,0 +1,24 @@
+/*
+JS => 
+    - synchronous
+    - single threaded (ek code execute, uske baad usra code)
+
+
+    Blocking Code vs Non-Blocking Code
+    block: block the flow of program
+    -read filoe sync
+
+    non-block : doesn't
+    - read file async (wait nhi krta
+
+
+        js Engine : Call stack & Memory Heap
+    )
+
+    setTimeOut( function (){}, 1000) => one Time
+    setInterval( function (){}, 1000) => repeat
+
+    dono queue me lagte call stack se alag nikal ke
+    async hai
+
+*/
